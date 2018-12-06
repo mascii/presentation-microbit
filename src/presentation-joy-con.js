5c4a93e04cab601d5a31db464afe46981682d497
Masaki Koyanagi <mascii@gmail.com>
2018-12-06 23:49:44 +0900

    change constants name

--- src/presentation-joy-con.js
+++ src/presentation-joy-con.js
@@ -3,7 +3,7 @@
     const BUTTON_SERVICE_UUID = 'e95d9882-251d-470a-a062-fa1922dfa9a8';
     const BUTTON_A_DATA_UUID = 'e95dda90-251d-470a-a062-fa1922dfa9a8';
     const BUTTON_B_DATA_UUID = 'e95dda91-251d-470a-a062-fa1922dfa9a8';
-    const [leftArrowKeyCode, rightArrowKeyCode] = [37, 39];
+    const [LEFT_ARROW_KEY_CODE, RIGHT_ARROW_KEY_CODE] = [37, 39];
 
     const pressKey = keyCode => {
         const activeElement = document.activeElement;
@@ -24,9 +24,9 @@
                     return;
                 }
                 if (charUUID === BUTTON_A_DATA_UUID) {
-                    pressKey(leftArrowKeyCode);
+                    pressKey(LEFT_ARROW_KEY_CODE);
                 } else if (charUUID === BUTTON_B_DATA_UUID) {
-                    pressKey(rightArrowKeyCode);
+                    pressKey(RIGHT_ARROW_KEY_CODE);
                 }
             });
     };
